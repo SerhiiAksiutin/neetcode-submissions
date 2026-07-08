8a715ee649047221fa5a9560b6006294c23889f8
SerhiiAksiutin <serhiiaksiutin@icloud.com>
2026-07-07 19:25:15 -0700

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        hash_map_s1 = {i:0 for i in range(97, 123)}
+        hash_map_s2 = {i:0 for i in range(97, 123)}
+
+        for i in s1:
+            hash_map_s1[ord(i)] = 1 + hash_map_s1.get(ord(i), 0)
+        # print(hash_map_s1)
+        l, r = 0, len(s1) - 1
+        for i in s2[l:r]:
+            hash_map_s2[ord(i)] = 1 + hash_map_s2.get(ord(i), 0)
+        while r < len(s2):
+            hash_map_s2[ord(s2[r])] += 1
+            if hash_map_s1 == hash_map_s2:
+                return True
+            r += 1
+            hash_map_s2[ord(s2[l])] -= 1
+            l += 1
+        
+        return False
+
+        
