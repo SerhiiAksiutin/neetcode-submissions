4725ed10703b9b73c9bcce19dedbcaa0710d9247
SerhiiAksiutin <serhiiaksiutin@icloud.com>
2026-08-26 17:14:39 -0700

    Add: validate-parentheses - submission-21

---
+++ Data Structures & Algorithms/validate-parentheses/submission-21.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        if len(s) < 2:
+            return False
+        stack = []
+        hash_map = {
+            "]":"[",
+            ")":"(",
+            "}":"{"
+        }
+
+        for c in s:
+            if c in hash_map.keys() and len(stack) != 0 and stack[-1] == hash_map[c]:
+                stack.pop()
+            else:
+                stack.append(c)
+        return stack == []
+
