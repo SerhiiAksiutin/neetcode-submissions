e8881a0344b344c059054e241ea70014ff0c1f3a
SerhiiAksiutin <serhiiaksiutin@icloud.com>
2026-08-30 22:42:17 -0700

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.py
@@ -0,0 +1,27 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    # [(0,30),(5,10),(15,20)]
+
+    # from 0 to 30
+    # from 5 to 10
+    # from 15 to 20
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        # for i in range(len(intervals)):
+        #     for j in range(i + 1, len(intervals)):
+        #         if intervals[i].start < intervals[j].end and intervals[i].end > intervals[j].start:
+        #             return False
+        # return True
+        intervals.sort(key=lambda i: i.start)
+        for i in range(len(intervals) - 1):
+            if intervals[i].end > intervals[i + 1].start:
+                return False
+        return True
+
+
